31f55ee750ced7e94b355acb4d9e27c3862103b1
Ha Phan Hai <ha.phanhai4869@hcmut.edu.vn>
2024-01-22 21:53:30 +0700

    Initial Commit

---
+++ src/Screens/Home/Home.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import { HStack, Spinner, Heading } from 'native-base';
+import { User } from '../../Services';
+import { Button, Text } from 'react-native-paper';
+
+export interface IHomeProps {
+  data: User | undefined;
+  isLoading: boolean;
+}
+
+export const Home = (props: IHomeProps) => {
+  const { data, isLoading } = props;
+  return (
+    <View style={styles.container}>
+      <StatusBar style='auto' />
+      {isLoading ? (
+        <HStack space={2} justifyContent='center'>
+          <Spinner accessibilityLabel='Loading posts' />
+        </HStack>
+      ) : (
+        <>
+          <Heading color='primary.500' fontSize='md'>
+            {data?.username}
+          </Heading>
+          <Text variant='displayLarge'>Display Large</Text>
+          <Button mode='contained'>abc</Button>
+        </>
+      )}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ src/Screens/index.ts
@@ -0,0 +1,6 @@
+export enum RootScreens {
+  MAIN = 'Main',
+  WELCOME = 'Welcome',
+  ONBOARDING = 'Onboarding',
+  NOT_FOUND = '*',
+}
