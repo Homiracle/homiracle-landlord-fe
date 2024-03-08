e82f4ff407fd20be7093b3c116d32a1d2f4b6cb7
Ha Phan Hai <ha.phanhai4869@hcmut.edu.vn>
2024-03-08 20:31:45 +0700

    chore: update title to mandatory and add component to header

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -4,21 +4,16 @@ import React, { useEffect, useState } from 'react';
 import { StyleSheet, View } from 'react-native';
 import { Appbar } from 'react-native-paper';
 import { CustomStatusBar } from '../StatusBar';
-import { getStatusBarHeight } from 'react-native-status-bar-height';
 
 export interface HeaderProps {
   children: React.ReactNode;
-  title?: string;
+  title: string;
   mode?: 'center-aligned' | 'medium' | 'small' | 'large';
   height?: number;
   top?: number | string;
   onBack?: () => void;
   onNotification?: () => void;
 }
-
-// write an handler for the back button
-
-// write an handler for the notification button
 
 export const Header: React.FC<HeaderProps> = ({
   children,
@@ -29,13 +24,6 @@ export const Header: React.FC<HeaderProps> = ({
   onNotification,
 }) => {
   const theme = useAppTheme();
-  // const [statusBarHeight, setStatusBarHeight] = useState(0);
-
-  // useEffect(() => {
-  //   const height = getStatusBarHeight();
-  //   setStatusBarHeight(height);
-  //   console.log('statusBarHeight', height);
-  // }, []);
 
   const styles = StyleSheet.create({
     container: {
@@ -54,7 +42,11 @@ export const Header: React.FC<HeaderProps> = ({
         {onBack && (
           <Appbar.BackAction onPress={onBack} color={theme.colors.onPrimary} />
         )}
-        <Appbar.Content title={title} color={theme.colors.onPrimary} />
+        <Appbar.Content
+          title={title}
+          color={theme.colors.onPrimary}
+          titleStyle={theme.fonts.titleLarge}
+        />
         {onNotification && (
           <Appbar.Action
             icon='bell-outline'
