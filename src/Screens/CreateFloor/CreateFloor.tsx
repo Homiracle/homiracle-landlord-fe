f0f0c84cadf50277c07efeeac1fff3faeaf3fd1c
Ha Phan Hai <ha.phanhai4869@hcmut.edu.vn>
2024-03-20 13:29:09 +0700

    feat: build UI for create floor

---
+++ src/Screens/CreateFloor/CreateFloor.tsx
@@ -0,0 +1,361 @@
+import React, { useEffect } from 'react';
+import { Text, View } from 'native-base';
+import { Header, CustomDialog } from '../../Components';
+import { useAppTheme } from '../../Theme';
+import { useNavigation } from '@react-navigation/native';
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from 'react-native-responsive-screen';
+import { StyleSheet, TextInput, TextStyle } from 'react-native';
+import { Button, Surface, Portal } from 'react-native-paper';
+import DateTimePicker from '@react-native-community/datetimepicker';
+import moment from 'moment';
+import {
+  RoomingHouse as RoomingHouseProps,
+  useCreateRoomingHouseMutation,
+} from '../../Services';
+import { useAppSelector } from '../../Store/hook';
+import { selectUserId } from '../../Store/reducers';
+import { roomingHouseFormValidationSchema as schema } from '../../Utils';
+import { useFormik } from 'formik';
+
+export const CreateFloor = () => {
+  // styles
+  const theme = useAppTheme();
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+    },
+    content: {
+      flex: 1,
+      flexDirection: 'column',
+      gap: hp(2),
+    },
+    surface: {
+      flex: 1,
+      flexDirection: 'column',
+      backgroundColor: theme.colors.onPrimary,
+      marginHorizontal: wp(2),
+      borderRadius: wp(2),
+      width: wp(96),
+      paddingHorizontal: wp(4),
+      paddingTop: hp(1),
+      paddingBottom: hp(2),
+    },
+    title: {
+      color: theme.colors.primary,
+      fontWeight: 'bold',
+    },
+    subTitle: {
+      fontWeight: 'bold',
+    },
+    textInput: {
+      borderBottomColor: theme.colors.primary,
+      borderBottomWidth: 1,
+      paddingVertical: hp(0.5),
+      color: 'black',
+    },
+    buttonContainer: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      marginHorizontal: wp(2),
+      marginVertical: hp(2),
+      gap: wp(2),
+    },
+    button: {
+      borderRadius: wp(2),
+      flex: 1,
+      height: hp(6),
+      justifyContent: 'center',
+    },
+    dropdown: {
+      borderBottomColor: theme.colors.primary,
+      borderBottomWidth: 1,
+    },
+    placeholderStyle: [
+      theme.fonts.bodyMedium,
+      {
+        opacity: 0.5,
+      },
+    ] as TextStyle,
+    goodText: {
+      color: 'green',
+    },
+    badText: {
+      color: 'red',
+    },
+  });
+
+  // other hooks
+  const navigation = useNavigation();
+  const [backDialog, showBackDialog] = React.useState(false);
+  const [cancelDialog, showCancelDialog] = React.useState(false);
+  const [roomingHouseData, setRoomingHouseData] =
+    React.useState<RoomingHouseProps>({
+      name: '',
+      opening_hour: '',
+      closing_hour: '',
+      number_of_period_days: 0,
+      closing_money_date: 0,
+      start_receiving_money_date: 0,
+      end_receiving_money_date: 0,
+      landlord: {
+        user_id: useAppSelector(selectUserId) as unknown as string | '',
+      },
+      address: {
+        province: '',
+        district: '',
+        commune: '',
+        street: '',
+      },
+      reference_cost: {
+        deposit: 0,
+        room_cost: 0,
+        water_cost: 0,
+        power_cost: 0,
+        // cost_per_person: 0,
+        // cost_per_room: 0,
+      },
+    });
+
+  const [createRoomingHouse, { data, error, isSuccess, isLoading, isError }] =
+    useCreateRoomingHouseMutation();
+
+  const formik = useFormik({
+    initialValues: roomingHouseData,
+    validationSchema: schema,
+    onSubmit: values => {
+      console.log(values);
+    },
+    validateOnChange: true,
+    validateOnBlur: true,
+    validateOnMount: true,
+  });
+
+  const onBack = () => {
+    showBackDialog(true);
+    console.log('back');
+  };
+
+  // console.log(useAppSelector(state => state.user));
+
+  const handleInputChange = (
+    fieldName: string,
+    text: string | number,
+    nestedField?: string,
+  ) => {
+    // console.log(fieldName, text);
+    if (nestedField) {
+      setRoomingHouseData(prevData => ({
+        ...prevData,
+        [fieldName]: {
+          ...prevData[fieldName],
+          [nestedField]: text,
+        },
+      }));
+      formik.handleChange(`${fieldName}.${nestedField}`)(String(text));
+    } else {
+      setRoomingHouseData(prevData => ({
+        ...prevData,
+        [fieldName]: text,
+      }));
+      formik.handleChange(fieldName)(String(text));
+    }
+  };
+
+  const handleSubmit = async () => {
+    console.log(roomingHouseData);
+    await createRoomingHouse(roomingHouseData as Partial<RoomingHouseProps>);
+    if (isSuccess) {
+      console.log(error);
+    } else if (isError) {
+      console.log('error', error);
+    }
+  };
+
+  const isTouched = (field: string, nestedField?: string) => {
+    if (nestedField) {
+      return (
+        formik.touched[field as keyof typeof formik.touched]?.[
+          nestedField as keyof (typeof formik.touched)[typeof field]
+        ] &&
+        formik.errors[field as keyof typeof formik.errors]?.[
+          nestedField as keyof (typeof formik.errors)[typeof field]
+        ]
+      );
+    } else {
+      return formik.touched[field] && formik.errors[field];
+    }
+  };
+
+  const onBlur = (field: string, nestedField?: string) => {
+    if (nestedField) {
+      return formik.setFieldTouched(field, {
+        ...(formik.touched[field] as any),
+        [nestedField]: true,
+      });
+    } else {
+      return formik.setFieldTouched(field, true);
+    }
+  };
+
+  // console.log(formik.touched);
+  // console.log(formik.errors);
+
+  // console.log(formik.isValid);
+
+  return (
+    <View style={styles.container}>
+      <Portal>
+        <CustomDialog
+          visible={backDialog}
+          title='Thoát'
+          content='Bạn có muốn thoát không?'
+          onDismiss={() => showBackDialog(false)}
+          onConfirm={() => {
+            showBackDialog(false);
+            navigation.goBack();
+          }}
+        />
+      </Portal>
+      <Portal>
+        <CustomDialog
+          visible={cancelDialog}
+          title='Hủy tạo nhà trọ'
+          content='Bạn có muốn hủy tạo nhà trọ không?'
+          onDismiss={() => showCancelDialog(false)}
+          onConfirm={() => {
+            showCancelDialog(false);
+            navigation.goBack();
+          }}
+        />
+      </Portal>
+      <Header
+        title='Tạo nhà trọ'
+        height={hp(8)}
+        mode='center-aligned'
+        onBack={onBack}
+        scroll='vertical'
+      >
+        <View style={styles.content}>
+          <Surface style={styles.surface}>
+            <Text style={[theme.fonts.titleMedium, styles.title]}>
+              Thông tin chung
+            </Text>
+            <View
+              style={{
+                flexDirection: 'column',
+                flex: 1,
+                gap: hp(2),
+                marginTop: hp(1),
+              }}
+            >
+              <View>
+                <Text style={styles.subTitle}>Tên tầng</Text>
+                <TextInput
+                  placeholder='Nhập tên tầng'
+                  style={styles.textInput}
+                  onChangeText={text => handleInputChange('name', text)}
+                  onBlur={() => onBlur('name')}
+                />
+                {isTouched('name') ? (
+                  <Text style={styles.badText}>{formik.errors.name}</Text>
+                ) : null}
+              </View>
+            </View>
+          </Surface>
+          <Surface style={styles.surface}>
+            <Text style={[theme.fonts.titleMedium, styles.title]}>
+              Chi phí tham khảo
+            </Text>
+            <View
+              style={{
+                flexDirection: 'column',
+                flex: 1,
+                gap: hp(2),
+                marginTop: hp(1),
+              }}
+            >
+              <View>
+                <Text style={styles.subTitle}>Tiền cọc tham khảo</Text>
+                <TextInput
+                  placeholder='Nhập tiền cọc tham khảo'
+                  style={styles.textInput}
+                  onChangeText={text =>
+                    handleInputChange('reference_cost', text, 'deposit')
+                  }
+                  keyboardType='numeric'
+                  onBlur={() => onBlur('reference_cost', 'deposit')}
+                />
+              </View>
+              <View>
+                <Text style={styles.subTitle}>Giá phòng tham khảo</Text>
+                <TextInput
+                  placeholder='Nhập giá phòng tham khảo'
+                  style={styles.textInput}
+                  onChangeText={text =>
+                    handleInputChange('reference_cost', text, 'room_cost')
+                  }
+                  keyboardType='numeric'
+                  onBlur={() => onBlur('reference_cost', 'room_cost')}
+                />
+              </View>
+              <View>
+                <Text style={styles.subTitle}>Giá điện tham khảo</Text>
+                <TextInput
+                  placeholder='Nhập giá điện tham khảo'
+                  style={styles.textInput}
+                  onChangeText={text =>
+                    handleInputChange('reference_cost', text, 'power_cost')
+                  }
+                  keyboardType='numeric'
+                  onBlur={() => onBlur('reference_cost', 'power_cost')}
+                />
+              </View>
+              <View>
+                <Text style={styles.subTitle}>Giá nước tham khảo</Text>
+                <TextInput
+                  placeholder='Nhập giá nước tham khảo'
+                  style={styles.textInput}
+                  onChangeText={text =>
+                    handleInputChange('reference_cost', text, 'water_cost')
+                  }
+                  keyboardType='numeric'
+                  onBlur={() => onBlur('reference_cost', 'water_cost')}
+                />
+              </View>
+              {/* <View>
+                <Text style={styles.subTitle}>Phí dịch vụ chung cho phòng</Text>
+              </View>
+              <View>
+                <Text style={styles.subTitle}>Phí dịch vụ theo đầu người</Text>
+              </View> */}
+            </View>
+          </Surface>
+        </View>
+        <View style={styles.buttonContainer}>
+          <Button
+            buttonColor={theme.colors.error}
+            textColor={theme.colors.onPrimary}
+            style={styles.button}
+            onPress={() => {
+              showCancelDialog(true);
+            }}
+          >
+            Hủy
+          </Button>
+          <Button
+            buttonColor={theme.colors.primary}
+            textColor={theme.colors.onPrimary}
+            style={styles.button}
+            onPress={handleSubmit}
+            disabled={!formik.isValid}
+          >
+            Tạo nhà trọ
+          </Button>
+        </View>
+      </Header>
+    </View>
+  );
+};
---
+++ src/Screens/CreateFloor/CreateFloorContainer.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { CreateFloor } from "./CreateFloor";
+
+export const CreateFloorContainer = () => {
+  return <CreateFloor />;
+};
