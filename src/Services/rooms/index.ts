49f3512a6decee415d8e6ba6aa24efaddea552f8
Ha Phan Hai <ha.phanhai4869@hcmut.edu.vn>
2024-03-22 00:37:42 +0700

    feat: create room

---
+++ src/Services/rooms/index.ts
@@ -0,0 +1,43 @@
+import { API } from '../base';
+
+export type Room = {
+  name: string;
+  acreage: number;
+  number_of_bedroom: number;
+  number_of_bathroom: number;
+  max_number_of_tenant?: number;
+  floor: {
+    floor_id: string;
+  };
+  reference_cost: {
+    deposit?: number;
+    room_cost?: number;
+    water_cost?: number;
+    power_cost?: number;
+    cost_per_person?: number;
+    cost_per_room?: number;
+  };
+  [key: string]: any;
+};
+
+export type RoomResponse = Partial<Room> & {
+  floor_id: string;
+};
+
+const RoomApi = API.injectEndpoints({
+  endpoints: build => ({
+    createRoom: build.mutation<RoomResponse, Partial<Room>>({
+      query: data => ({
+        url: 'Rooms',
+        method: 'POST',
+        body: data,
+      }),
+    }),
+    getRooms: build.query<Room[], void>({
+      query: id => `rooms/${id}`,
+    }),
+  }),
+  overrideExisting: true,
+});
+
+export const { useCreateRoomMutation, useGetRoomsQuery } = RoomApi;
