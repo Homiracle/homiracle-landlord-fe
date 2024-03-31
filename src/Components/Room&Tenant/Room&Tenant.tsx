1253d4db43cb729ff25922c352b88a6f3926c598
Ha Phan Hai <ha.phanhai4869@hcmut.edu.vn>
2024-03-31 17:19:45 +0700

    format: change tab spaces to 2

--- src/Components/Room&Tenant/Room&Tenant.tsx
+++ src/Components/Room&Tenant/Room&Tenant.tsx
@@ -1,124 +1,110 @@
-import { useAppTheme } from "../../Theme"
-import React from "react";
-import { Text, View } from "react-native";
-import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
-import Icon from "react-native-vector-icons/MaterialCommunityIcons";
+import { useAppTheme } from '../../Theme';
+import React from 'react';
+import { Text, View } from 'react-native';
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from 'react-native-responsive-screen';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 export interface RoomAndTenantProps {
-    num_of_room?: number,
-    num_of_tenant?: number,
+  num_of_room?: number;
+  num_of_tenant?: number;
 }
 
 export const RoomAndTenant = ({
-    num_of_room,
-    num_of_tenant,
+  num_of_room,
+  num_of_tenant,
 }: RoomAndTenantProps) => {
-    const homiralceTheme = useAppTheme();
-    return (
+  const homiralceTheme = useAppTheme();
+  return (
+    <View
+      style={{
+        display: 'flex',
+        width: wp('100%'),
+        flexDirection: 'row',
+        justifyContent: 'center',
+        gap: wp('2%'),
+        alignSelf: 'center',
+      }}
+    >
+      <View
+        style={{
+          width: wp('40%'),
+          height: 68,
+          borderRadius: 10,
+          backgroundColor: 'white',
+          alignItems: 'center',
+          flexDirection: 'row',
+          gap: 10,
+        }}
+      >
         <View
-            style={{
-                display: 'flex',
-                width: wp('100%'),
-                flexDirection: 'row',
-                justifyContent: 'center',
-                gap: wp('2%'),
-                alignSelf: 'center',
-        }}>
-            <View 
-            style={{
-                width: wp('40%'),
-                height: 68,
-                borderRadius: 10,
-                backgroundColor: 'white',
-                alignItems: 'center',
-                flexDirection: 'row',
-                gap: 10,
-            }}
-            >
-                <View
-                    style={{
-                        paddingLeft: 18,
-                        width: 90
-                    }}>
-                <Text 
-                style={[
-                    homiralceTheme.fonts.titleMedium
-                ]}
-                >{num_of_tenant? num_of_tenant: 0}</Text>
+          style={{
+            paddingLeft: 18,
+            width: 90,
+          }}
+        >
+          <Text style={[homiralceTheme.fonts.titleMedium]}>
+            {num_of_tenant ? num_of_tenant : 0}
+          </Text>
 
-                <Text
-                style={[
-                   homiralceTheme.fonts.bodySmall
-                ]}
-                >Phòng trống</Text>
-                </View>
-                
-
-                <View
-                    style={{
-                        display: 'flex',
-                        justifyContent: 'center',
-                        alignItems: 'center',
-                        width: 48,
-                        height: 48,
-                        borderRadius: 24,
-                        backgroundColor: '#6FFBBE',
-                    }}>
-                    <Icon 
-                        name='home-variant'
-                        size={24}
-                        color='white'
-                    />
-                </View>
-            </View>
-        
-            <View
-            style={{
-                width: wp('40%'),
-                height: 68,
-                borderRadius: 10,
-                backgroundColor: 'white',
-                alignItems: 'center',
-                flexDirection: 'row',
-                gap: 10,
-            }}>
-                <View
-                    style={{
-                        paddingLeft: 18,
-                        width: 90,
-                    }}>
-                <Text 
-                style={[
-                    homiralceTheme.fonts.titleMedium
-                ]}
-                >{num_of_room? num_of_room: 0}</Text>
-
-                <Text
-                style={[
-                    homiralceTheme.fonts.bodySmall
-                ]}
-                >Người ở</Text>
-                </View>
-
-                <View
-                    style={{
-                        display: 'flex',
-                        justifyContent: 'center',
-                        alignItems: 'center',
-                        width: 48,
-                        height: 48,
-                        borderRadius: 24,
-                        backgroundColor: '#FFDBCB',
-                    }}>
-                    <Icon 
-                        name='account-multiple'
-                        size={24}
-                        color='white'
-                    />
-                </View>
-                
-            </View>
+          <Text style={[homiralceTheme.fonts.bodySmall]}>Phòng trống</Text>
         </View>
-        
-    )
-} 
+
+        <View
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            width: 48,
+            height: 48,
+            borderRadius: 24,
+            backgroundColor: '#6FFBBE',
+          }}
+        >
+          <Icon name='home-variant' size={24} color='white' />
+        </View>
+      </View>
+
+      <View
+        style={{
+          width: wp('40%'),
+          height: 68,
+          borderRadius: 10,
+          backgroundColor: 'white',
+          alignItems: 'center',
+          flexDirection: 'row',
+          gap: 10,
+        }}
+      >
+        <View
+          style={{
+            paddingLeft: 18,
+            width: 90,
+          }}
+        >
+          <Text style={[homiralceTheme.fonts.titleMedium]}>
+            {num_of_room ? num_of_room : 0}
+          </Text>
+
+          <Text style={[homiralceTheme.fonts.bodySmall]}>Người ở</Text>
+        </View>
+
+        <View
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            width: 48,
+            height: 48,
+            borderRadius: 24,
+            backgroundColor: '#FFDBCB',
+          }}
+        >
+          <Icon name='account-multiple' size={24} color='white' />
+        </View>
+      </View>
+    </View>
+  );
+};
