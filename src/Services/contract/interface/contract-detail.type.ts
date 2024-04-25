d687d682f648bc83aa38a5a27d335e497fd9583a
Bao Pham Le <baophamst@gmail.com>
2024-04-25 16:17:02 +0700

    Contract Detail with API, Change type Contract Detail

--- src/Services/contract/interface/contract-detail.type.ts
+++ src/Services/contract/interface/contract-detail.type.ts
@@ -1,19 +1,41 @@
 export type ContractDetails = {
-    room_id: string;
-    contract_id: number;
-    start_date: string;
+    contract_id: string;
+    cost: {
+        cost_per_person: number;
+        cost_per_room: number;
+        deposit: number;
+        power_cost: number;
+        room_cost: number;
+        water_cost: number;
+    };
+    couting_fee_day: string;
+    createdAt: string;
     end_date: string;
-    couting_fee_day:string;
-    paying_cost_cycle: string;
-    maximmum_number_of_people: number;
-    reference_cost: {
-        deposit?: number;
-        room_cost?: number;
-        water_cost?: number;
-        power_cost?: number;
-        cost_per_person?: number;
-        cost_per_room?: number;
-      };
-    tenant_id?: string;
+    landlord: {
+        CID: null | string;
+        email: string;
+        phone: string;
+        user_id: string;
+        user_name: string;
+    };
+    maximum_number_of_peoples: number;
+    paying_cost_cycle: number;
+    room: {
+        name: string;
+        rooming_house: {
+            address: Record<string, any>; // Assuming the structure of address is not known
+            name: string;
+        };
+    };
+    start_date: string;
     status: string;
-}
+    tenant: {
+        CID: null | string;
+        email: string;
+        phone: string;
+        user_id: string;
+        user_name: string;
+    };
+    updatedAt: string;
+};
+
