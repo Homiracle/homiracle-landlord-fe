7390d3ff69fbb79f0a8754897609f0e661bd494d
Bao Pham Le <baophamst@gmail.com>
2024-06-02 15:06:20 +0700

    Constraint: Room Area, StartDate < Enđate, counting_fee day must be [1..30]

--- src/Utils/Validation/contractSchema.ts
+++ src/Utils/Validation/contractSchema.ts
@@ -1,16 +1,20 @@
 import * as Yup from 'yup';
 
 export const contractFormValidationSchema = Yup.object().shape({
-        start_date: Yup.string().required(),
-        end_date:Yup.string().required(),
-        couting_fee_day:Yup.string().required(),
-        paying_cost_cycle: Yup.number().required(),
-        reference_cost: Yup.object({
-            deposit: Yup.number().optional().positive(),
-            room_cost:Yup.number().positive(),
-            water_cost: Yup.number().positive(),
-            power_cost:Yup.number().positive(),
-          }),
-
-    // cost_per_room: Yup.number().positive(),
+  start_date: Yup.string().required(),
+  end_date: Yup.string().required().test('end-date', 'Ngày kết thúc phải sau ngày bắt đầu', function(value) {
+    const { start_date } = this.parent;
+    return new Date(value) > new Date(start_date);
+  }),
+  couting_fee_day: Yup.string().required().test('count_day', 'Ngày tính phí phải là một ngày trong tháng', function(value) {
+    const date = Number(value);
+    return date > 0 && date < 32; 
+  }),
+  paying_cost_cycle: Yup.number().required(),
+  reference_cost: Yup.object({
+    deposit: Yup.number().optional().positive(),
+    room_cost: Yup.number().positive(),
+    water_cost: Yup.number().positive(),
+    power_cost: Yup.number().positive(),
+  }),
 });
--- src/Utils/Validation/roomSchema.ts
+++ src/Utils/Validation/roomSchema.ts
@@ -4,6 +4,7 @@ export const roomFormValidationSchema = Yup.object().shape({
   name: Yup.string().required('Tên phòng không được để trống'),
   number_of_bedroom: Yup.number().required('Số phòng ngủ không được để trống'),
   number_of_bathroom: Yup.number().required('Số phòng tắm không được để trống'),
+  acreage: Yup.number().max(300,'Diện tích quá lớn vui lòng kiểm tra lại'),
   reference_cost: Yup.object({
     deposit: Yup.number().optional().positive(),
     room_cost: Yup.number().optional().positive(),
